0c8f831e8ebe4e733b0d9bd2ec95e23abf4e0788
VladimirtheGreatest <vladkodoktorov@gmail.com>
2020-02-08 19:51:54 +0000

    second commit added script and assets

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    //I need to know what object are clickable
+    public LayerMask clickableLayer;
+    //swap cursors per objects
+    public Texture2D pointer; //normal pointer
+    public Texture2D target; //cursor for clickable objects like the world
+    public Texture2D doorway; // cursor for doorways
+    public Texture2D combat; //cursor combat actions
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+        {
+            bool door = false;
+            if (hit.collider.gameObject.tag == "Doorway")
+            {
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+            }
+            else
+            {
+                Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+            }
+
+
+        }
+        else
+        {
+            Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
